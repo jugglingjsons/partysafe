e0a3dd79be8f2bcaaf3bcede483288d1c7f11a0c
Smail Bensaad <amsmailbensaad@gmail.com>
2023-08-30 17:06:09 +0200

    partysafe

---
+++ src/components/ProductBox.js
@@ -0,0 +1,12 @@
+// function ProductBox({ product }) {
+//     return (
+//         <div>
+//             <img src={product.image} alt={product.name} />
+//             <h2>{product.name}</h2>
+//             <p>{product.price}€</p>
+//             <button>♥</button>
+//         </div>
+//     );
+// }
+
+// export default ProductBox;
